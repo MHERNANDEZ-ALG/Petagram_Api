1ae4e1aafc552ee3521b2508f697f77dd870b97b
Miguel Angel Hernandez <mahb880@gmail.com>
2020-10-09 00:18:14 -0300

    Cambios actividad semana 4, se agrego envio de mail de contacto

--- app/src/main/java/algorithm/mhernandez/petagram/FormActivity.java
+++ app/src/main/java/algorithm/mhernandez/petagram/FormActivity.java
@@ -8,13 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import javax.mail.Session;
 
 public class FormActivity extends AppCompatActivity {
 
     String correo = "mhtestscode@gmail.com";
-    String contraseña;
-    Session session;
+    String pass;
 
     EditText etMensaje;
     EditText etNombre;
@@ -25,22 +23,22 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         correo = "mhtestscode@gmail.com";
-        contraseña = "mhtc.1991";
-        etMensaje = (EditText) findViewById(R.id.txtMensaje);
-        etNombre = (EditText) findViewById(R.id.txtNombre);
-        etCorreo = (EditText) findViewById(R.id.txtEmail);
+        pass = "mhtc.1991";
+        etMensaje = findViewById(R.id.txtMensaje);
+        etNombre = findViewById(R.id.txtNombre);
+        etCorreo = findViewById(R.id.txtEmail);
 
-        button = (Button) findViewById(R.id.btnEnviarCom);
+        button = findViewById(R.id.btnEnviarCom);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Toast.makeText(FormActivity.this,"Comentario Enviado",Toast.LENGTH_LONG).show();
                 String correorem = etCorreo.getText().toString();
                 String nombreRe = etNombre.getText().toString();
                 String contenido = etMensaje.getText().toString();
-                new SendMail(correo, contraseña).execute(new SendMail.Mail(
+                new SendMail(correo, pass).execute(new SendMail.Mail(
                         "App_Petagram", correo,
                                 "Mensaje de: " + nombreRe + correorem, contenido));
                 etCorreo.setText("");
